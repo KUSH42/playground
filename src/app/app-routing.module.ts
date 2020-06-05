e7deb7bceb59517e8f9538e31cf36e00821e0c68
Marc <mr.fak9r@gmail.com>
2020-06-06 00:09:56 +0200

    Adjusted routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { ParticlesConnectComponent } from './particles-connect/particles-connect.component';
+
 import { CssSnippetsComponent } from './css-snippets/css-snippets.component';
+import { HomeComponent }from './home/home.component';
 import { ParticleBannerComponent } from './particle-banner/particle-banner.component';
+import { ParticlesConnectComponent } from './particles-connect/particles-connect.component';
 
 const appRoutes: Routes = [
-  { path: 'particles-connect', component: ParticlesConnectComponent },
+  { path: '', component: HomeComponent },
   { path: 'css-snippets', component: CssSnippetsComponent },
-  { path: 'particle-banner', component: ParticleBannerComponent }
+  { path: 'particle-banner', component: ParticleBannerComponent },
+  { path: 'particles-connect', component: ParticlesConnectComponent }
 ];
 
 @NgModule({
