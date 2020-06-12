5959dddf163fc0018339c6fb9d8113e81b588d52
Marc <mr.fak9r@gmail.com>
2020-06-12 21:36:11 +0200

    added global color buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { GlobalManagerService } from './global-manager.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'playground';
   settings = {
-    expanded: true
+    expanded: true,
   };
+  appropriateClass: string = '';
+  selectedColor: string = 'green';
 
   public log() {
     console.log(this.settings.expanded);
@@ -17,5 +20,22 @@ export class AppComponent {
 
   public onCollapseButtonClick(state: boolean) {
     this.settings.expanded = !this.settings.expanded;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenHeight(event?) {
+    //console.log(window.innerHeight);
+    if (window.innerHeight <= 412) {
+      this.appropriateClass = 'bottomRelative';
+    } else {
+      this.appropriateClass = 'bottomStick';
+    }
+  }
+  constructor(private _GlobalManagerSerice:GlobalManagerService) {
+    this.getScreenHeight();
+  }
+
+  onColorChange(value:string) {
+    this._GlobalManagerSerice.selectedColor = value;
   }
 }
