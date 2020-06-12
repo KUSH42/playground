4de6546fe47e655b66011851cf2b6da69425e051
Marc <mr.fak9r@gmail.com>
2020-06-13 00:38:13 +0200

    refactoring

--- src/app/particles-connect/particles-connect.component.ts
+++ src/app/particles-connect/particles-connect.component.ts
@@ -1,30 +1,42 @@
-import { Component, ViewChild, AfterViewInit, ElementRef, OnDestroy } from "@angular/core";
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  OnDestroy,
+} from '@angular/core';
 
-import * as THREE from "three";
+import * as THREE from 'three';
 import * as dat from 'dat.gui';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component({
-  selector: "app-particles-connect",
-  templateUrl: "./particles-connect.component.html",
-  styleUrls: ["./particles-connect.component.css"],
+  selector: 'app-particles-connect',
+  templateUrl: './particles-connect.component.html',
+  styleUrls: ['./particles-connect.component.css'],
 })
-export class ParticlesConnectComponent implements AfterViewInit, OnDestroy {
-  @ViewChild("rendererContainer")
-  private container: ElementRef;
-  private group: any;
-  private particlesData = [];
-  private camera: any;
-  private scene: any;
-  private renderer = new THREE.WebGLRenderer({ antialias: true });
-  private positions: any;
-  private colors: any;
 
-  private particles: THREE.BufferGeometry;
-  private pointCloud: any;
-  private particlePositions: any;
-  private linesMesh: any;
+export class ParticlesConnectComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('rendererContainer')
+  private container: ElementRef;
+
+  private scene: THREE.Scene;
+  private renderer: THREE.WebGLRenderer;
   private gui: dat.GUI;
+  private camera: THREE.PerspectiveCamera;
+
+  private group: THREE.Group;
+  private linesMesh: THREE.LineSegments;
+  private particles: THREE.BufferGeometry;
+  private particlesData: {
+    velocity: THREE.Vector3;
+    numConnections: number;
+  }[] = [];
+  private particlePositions: Float32Array;
+  private pointCloud: any;
+  private positions: Float32Array;
+  private colors: Float32Array;
 
   public maxParticleCount = 1000;
   private r = 800;
@@ -43,8 +55,6 @@ export class ParticlesConnectComponent implements AfterViewInit, OnDestroy {
   constructor() {}
 
   ngAfterViewInit() {
-    this.renderer.setSize(this.container.nativeElement.offsetWidth, this.container.nativeElement.offsetHeight);
-    this.container.nativeElement.appendChild(this.renderer.domElement);
     this.init();
     this.animate();
   }
@@ -53,60 +63,63 @@ export class ParticlesConnectComponent implements AfterViewInit, OnDestroy {
     this.gui.destroy();
   }
 
+  private init() {
+    this.initGUI();
+    this.initCamera();
+    this.initOrbitalControls();
+    this.initScene();
+    this.initParticles();
+    this.initRenderer();
+
+    this.container.nativeElement.appendChild(this.renderer.domElement);
+    window.addEventListener('resize', this.onWindowResize.bind(this), false);
+  }
+
   private initGUI() {
     this.gui = new dat.GUI();
     this.gui.close();
-    this.gui.add(this.effectController, "showDots");
-    this.gui.add(this.effectController, "showLines");
-    this.gui.add(this.effectController, "minDistance", 10, 300);
-    this.gui.add(this.effectController, "limitConnections");
-    this.gui.add(this.effectController, "maxConnections", 0, 30, 1);
+    this.gui.add(this.effectController, 'showDots');
+    this.gui.add(this.effectController, 'showLines');
+    this.gui.add(this.effectController, 'minDistance', 10, 300);
+    this.gui.add(this.effectController, 'limitConnections');
+    this.gui.add(this.effectController, 'maxConnections', 0, 30, 1);
     this.gui.add(
       this.effectController,
-      "particleCount",
+      'particleCount',
       0,
       this.maxParticleCount,
       1
     );
-    this.gui.addColor(this.effectController, "color");
+    this.gui.addColor(this.effectController, 'color');
   }
 
-  private init() {
-    this.initGUI();
-
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-     (this.container.nativeElement.offsetWidth / this.container.nativeElement.offsetHeight),
-      1,
-      4000
-    );
+  private initCamera() {
+    this.camera = new THREE.PerspectiveCamera(45, this.container.nativeElement.offsetWidth /
+      this.container.nativeElement.offsetHeight, 1, 4000);
     this.camera.position.z = 1750;
+  }
 
-    const controls = new OrbitControls(
-      this.camera,
-      this.container.nativeElement
-    );
+  private initOrbitalControls() {
+    const controls = new OrbitControls(this.camera, this.container.nativeElement);
     controls.minDistance = 1000;
     controls.maxDistance = 3000;
+  }
 
+  private initScene() {
     this.scene = new THREE.Scene();
-
     this.group = new THREE.Group();
     this.scene.add(this.group);
+  }
 
-    const helper = new THREE.BoxHelper(
-      new THREE.Mesh(new THREE.BoxBufferGeometry(this.r, this.r, this.r))
-    );
+  private initParticles() {
+    const helper = new THREE.BoxHelper(new THREE.Mesh(new THREE.BoxBufferGeometry(this.r, this.r, this.r)));
     (<any>helper.material).color.setHex(0x101010);
     (<any>helper.material).blending = THREE.AdditiveBlending;
     (<any>helper.material).transparent = true;
     this.group.add(helper);
-
     const segments = this.maxParticleCount * this.maxParticleCount;
-
     this.positions = new Float32Array(segments * 3);
     this.colors = new Float32Array(segments * 3);
-
     const pMaterial = new THREE.PointsMaterial({
       color: 0x101010,
       size: 3,
@@ -114,83 +127,58 @@ export class ParticlesConnectComponent implements AfterViewInit, OnDestroy {
       transparent: true,
       sizeAttenuation: false,
     });
-
     this.particles = new THREE.BufferGeometry();
     this.particlePositions = new Float32Array(this.maxParticleCount * 3);
-
     for (let i = 0; i < this.maxParticleCount; i++) {
       let x = Math.random() * this.r - this.r / 2;
       let y = Math.random() * this.r - this.r / 2;
       let z = Math.random() * this.r - this.r / 2;
-
       this.particlePositions[i * 3] = x;
       this.particlePositions[i * 3 + 1] = y;
       this.particlePositions[i * 3 + 2] = z;
-
       // add it to the geometry
       this.particlesData.push({
-        velocity: new THREE.Vector3(
-          -1 + Math.random() * 2,
-          -1 + Math.random() * 2,
-          -1 + Math.random() * 2
-        ),
+        velocity: new THREE.Vector3(-1 + Math.random() * 2, -1 + Math.random() * 2, -1 + Math.random() * 2),
         numConnections: 0,
       });
     }
-
     this.particles.setDrawRange(0, this.effectController.particleCount);
-    this.particles.setAttribute(
-      "position",
-      new THREE.BufferAttribute(this.particlePositions, 3).setUsage(
-        THREE.DynamicDrawUsage
-      )
-    );
-
+    this.particles.setAttribute('position', new THREE.BufferAttribute(this.particlePositions, 3).setUsage(THREE.DynamicDrawUsage));
     // create the particle system
     this.pointCloud = new THREE.Points(this.particles, pMaterial);
     this.group.add(this.pointCloud);
-
     const geometry = new THREE.BufferGeometry();
-
-    geometry.setAttribute(
-      "position",
-      new THREE.BufferAttribute(this.positions, 3).setUsage(
-        THREE.DynamicDrawUsage
-      )
-    );
-    geometry.setAttribute(
-      "color",
-      new THREE.BufferAttribute(this.colors, 3).setUsage(THREE.DynamicDrawUsage)
-    );
-
+    geometry.setAttribute('position', new THREE.BufferAttribute(this.positions, 3).setUsage(THREE.DynamicDrawUsage));
+    geometry.setAttribute('color', new THREE.BufferAttribute(this.colors, 3).setUsage(THREE.DynamicDrawUsage));
     geometry.computeBoundingSphere();
-
     geometry.setDrawRange(0, 0);
-
     const material = new THREE.LineBasicMaterial({
       color: this.effectController.color,
       vertexColors: true,
       blending: THREE.AdditiveBlending,
       transparent: true,
     });
-
     this.linesMesh = new THREE.LineSegments(geometry, material);
     this.group.add(this.linesMesh);
+  }
 
+  private initRenderer() {
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(this.container.nativeElement.offsetWidth, this.container.nativeElement.offsetHeight);
     this.renderer.outputEncoding = THREE.sRGBEncoding;
-
-    this.container.nativeElement.appendChild(this.renderer.domElement);
-
-    window.addEventListener("resize", this.onWindowResize.bind(this), false);
   }
 
   private onWindowResize() {
-    this.camera.aspect =(this.container.nativeElement.offsetWidth / this.container.nativeElement.offsetHeight);
+    this.camera.aspect =
+      this.container.nativeElement.offsetWidth /
+      this.container.nativeElement.offsetHeight;
     this.camera.updateProjectionMatrix();
 
-    this.renderer.setSize(this.container.nativeElement.offsetWidth, this.container.nativeElement.offsetHeight);
+    this.renderer.setSize(
+      this.container.nativeElement.offsetWidth,
+      this.container.nativeElement.offsetHeight
+    );
   }
 
   private animate() {
@@ -282,13 +270,13 @@ export class ParticlesConnectComponent implements AfterViewInit, OnDestroy {
       }
     }
 
-    this.linesMesh.material.color = new THREE.Color(
+    (<any>this.linesMesh.material).color = new THREE.Color(
       this.effectController.color
     );
 
-    this.linesMesh.geometry.setDrawRange(0, numConnected * 2);
-    this.linesMesh.geometry.attributes.position.needsUpdate = true;
-    this.linesMesh.geometry.attributes.color.needsUpdate = true;
+    (<any>this.linesMesh.geometry).setDrawRange(0, numConnected * 2);
+    (<any>this.linesMesh.geometry).attributes.position.needsUpdate = true;
+    (<any>this.linesMesh.geometry).attributes.color.needsUpdate = true;
     this.pointCloud.geometry.attributes.position.needsUpdate = true;
 
     requestAnimationFrame(this.animate.bind(this));
