bc7f0bf7272da37ad3a236bae6e8e91a31d0e0f7
Marc <mr.fak9r@gmail.com>
2020-06-14 17:53:51 +0200

    removed some characters from matrix animation

--- src/app/components/matrix/matrix.component.ts
+++ src/app/components/matrix/matrix.component.ts
@@ -19,7 +19,7 @@ export class MatrixComponent implements AfterViewInit, OnDestroy {
 
   private drops: number[] = []; // an array of drops - one per column
   // random characters - taken from the unicode charset
-  private chars = 'АВЕДЁЗИЛМНОЯЩЦХТСРП漢字は日本語で使われる文字で中国からやってきました。ひらがなとカタカナは日本で作られました©®¶$#!(*<=>?@¥ͶΔΛΞΠΣΩΨΧ☺ت0123456798ABCDEF田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑'.split(
+  private chars = 'АВЕДЁЗИЛМНОЯЩЦХТСРП漢字は日本語で使われる文字で中国からやってきました。ひらがなとカタカナは日本で作られました©®¶$#!(*<=>?@¥ͶΔΛΞΠΣΩΨΧ0123456798ABCDEF'.split(
     ''
   );
   // array of colors to randomize characters with
