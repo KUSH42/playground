7775d625ec734131bc6680fb2862d166effa74a3
Marc <mr.fak9r@gmail.com>
2020-06-20 00:51:17 +0200

    fixed target-focusing

--- src/app/components/globe/globe.component.ts
+++ src/app/components/globe/globe.component.ts
@@ -17,7 +17,7 @@ export class GlobeComponent implements AfterViewInit {
   private container: ElementRef;
   @ViewChild('canvas', { read: ElementRef, static: false })
   private canvas: ElementRef;
-  // Canvas width and height
+
   private width = 1000;
   private height = 800;
   private globeRadius = 200;
@@ -55,9 +55,9 @@ export class GlobeComponent implements AfterViewInit {
     },
   };
 
-  // A state object to hold visual state.
+  // Visual state
   private state = {
-    users: [
+    targets: [
       {
         id: 0,
         name: 'John Yang',
@@ -89,10 +89,11 @@ export class GlobeComponent implements AfterViewInit {
         date: '12.25.2018',
       },
     ],
-    currentUserIndex: null,
-    previousUserIndex: null,
-    isGlobeAnimating: false,
-    // Property to save our setInterval id to auto rotate the globe every n seconds
+    currentTargetIndex: null,
+    previousTargetIndex: null,
+    isGlobeAnimating: true,
+
+    // Property to save setInterval id to auto rotate the globe every n seconds
     autoRotateGlobeTimer: null,
   };
 
@@ -147,7 +148,6 @@ export class GlobeComponent implements AfterViewInit {
   }
 
   private setup() {
-    // Setup our Scene, Camera, and Renderer
     this.scene = new THREE.Scene();
     this.camera.object = new THREE.PerspectiveCamera(
       45,
@@ -168,7 +168,8 @@ export class GlobeComponent implements AfterViewInit {
     this.setupOrbitControls();
     this.setupAutoRotate();
     this.render();
-    // subscribe to global color change
+
+    // Subscribe to global color change
     this.subscriptions.push(
       this._GlobalManagerSerice.colorObservable.subscribe((color: string) => {
         this.selectedColor = color;
@@ -242,11 +243,7 @@ export class GlobeComponent implements AfterViewInit {
       this.camera.object,
       this.canvas.nativeElement
     );
-    /* this.camera.orbitControls.enableKeys = false;
-    this.camera.orbitControls.enablePan = false;
-    this.camera.orbitControls.enableZoom = false;
-    this.camera.orbitControls.enableDamping = false;
-    this.camera.orbitControls.enableRotate = false;*/
+
     this.camera.object.position.z = -550;
     this.camera.orbitControls.update();
   }
@@ -258,18 +255,17 @@ export class GlobeComponent implements AfterViewInit {
   }
 
   private focusUser() {
-    if (this.state.users.length > 0) {
-      if (this.state.currentUserIndex === null) {
-        // If there is no current user (when our page first loads), we'll pick one randomly.
-        this.state.currentUserIndex = this.getRandomNumberBetween(
+    if (this.state.targets.length > 0) {
+      if (this.state.currentTargetIndex === null) {
+        // If there is no current target, pick one randomly
+        this.state.currentTargetIndex = this.getRandomNumberBetween(
           0,
-          this.state.users.length - 1
+          this.state.targets.length - 1
         );
       } else {
-        // If we already have an index (page has already been loaded/user already clicked next), we'll continue the sequence.
-        this.state.previousUserIndex = this.state.currentUserIndex;
-        this.state.currentUserIndex =
-          (this.state.currentUserIndex + 1) % this.state.users.length;
+        this.state.previousTargetIndex = this.state.currentTargetIndex;
+        this.state.currentTargetIndex =
+          (this.state.currentTargetIndex + 1) % this.state.targets.length;
       }
 
       this.focusGlobe();
@@ -277,19 +273,18 @@ export class GlobeComponent implements AfterViewInit {
   }
 
   private focusGlobe() {
-    // 1. We'll get the current user's lat/lng
-    // 2. Set camera.angles.current
-    // 3. Calculate and set camera.angles.target
-    // 4. animate method will handle animating
-    const { geo } = this.state.users[this.state.currentUserIndex];
+    const { geo } = this.state.targets[this.state.currentTargetIndex];
+
     this.camera.angles.current.azimuthal = this.camera.orbitControls.getAzimuthalAngle();
     this.camera.angles.current.polar = this.camera.orbitControls.getPolarAngle();
+
     const { x, y } = this.convertLatLngToFlatCoords(geo.lat, geo.lng);
+
     const { azimuthal, polar } = this.returnCameraAngles(x, y);
+
     this.camera.angles.target.azimuthal = azimuthal;
     this.camera.angles.target.polar = polar;
-    // Updating state here will make sure our animate method will rotate our globe to the next point.
-    // It will also make sure we update & cache our popup DOM element so we can use it in our animateGlobeToNextLocation.
+
     this.state.isGlobeAnimating = true;
   }
 
@@ -301,19 +296,11 @@ export class GlobeComponent implements AfterViewInit {
 
   private animate() {
     if (this.state.isGlobeAnimating) {
-      // Here we update azimuthal and polar angles.
-      // Our focusGlobe() method will trigger state.isGlobeAnimating
-      // and update the current and target azimuthal/polar angles of
-      // our camera. Then, animateGlobeToNextLocation() will
-      // animate globe from the current azimuthal/polar angles to the target.
-      // It will then set state.isGlobeAnimating to false when the angles are equal.
-      //this.animateGlobeToNextLocation()
-      console.log(this.camera);
-      this.state.isGlobeAnimating = false;
+      // Update azimuthal and polar angles
+      this.animateGlobeToNextLocation()
       this.camera.orbitControls.update();
+      return;
     }
-    this.groups.globe.rotation.y += 0.005;
-    this.groups.globePoints.rotation.y += 0.005;
   }
 
   private animateGlobeToNextLocation() {
@@ -324,15 +311,16 @@ export class GlobeComponent implements AfterViewInit {
         current: { azimuthal: currentAzimuthal, polar: currentPolar },
         target: { azimuthal: targetAzimuthal, polar: targetPolar },
       } = this.camera.angles;
-      var azimuthalDifference = (currentAzimuthal - targetAzimuthal) * progress;
+      let azimuthalDifference = (currentAzimuthal - targetAzimuthal) * progress;
       azimuthalDifference = currentAzimuthal - azimuthalDifference;
-      this.camera.orbitControls.minAzimuthalAngle = azimuthalDifference;
-      this.camera.orbitControls.maxAzimuthalAngle = azimuthalDifference;
-      var polarDifference = (currentPolar - targetPolar) * progress;
+      this.camera.orbitControls.minAzimuthAngle = azimuthalDifference;
+      this.camera.orbitControls.maxAzimuthAngle = azimuthalDifference;
+      let polarDifference = (currentPolar - targetPolar) * progress;
       polarDifference = currentPolar - polarDifference;
       this.camera.orbitControls.minPolarAngle = polarDifference;
       this.camera.orbitControls.maxPolarAngle = polarDifference;
       this.camera.transition.current++;
+
     } else {
       this.state.isGlobeAnimating = false;
       this.camera.transition.current = 0;
@@ -349,12 +337,13 @@ export class GlobeComponent implements AfterViewInit {
   }
 
   private convertFlatCoordsToSphereCoords(x, y) {
-    // Calculate the relative 3d coordinates using Mercator projection relative to the radius of the globe.
-    // Convert latitude and longitude on the 90/180 degree axis.
+    // Calculate relative 3d coordinates using Mercator projection relative to the radius of the globe
+    // Convert latitude and longitude on the 90/180 degree axis
     let latitude = ((x - this.globeWidth) / this.globeWidth) * -180;
     let longitude = ((y - this.globeHeight) / this.globeHeight) * -90;
     latitude = (latitude * Math.PI) / 180; //(latitude / 180) * Math.PI
-    longitude = (longitude * Math.PI) / 180; //(longitude / 180) * Math.PI // Calculate the projected starting point
+    longitude = (longitude * Math.PI) / 180; //(longitude / 180) * Math.PI
+    // Calculate projected starting point
     const radius = Math.cos(longitude) * this.globeRadius;
     const targetX = Math.cos(latitude) * radius;
     const targetY = Math.sin(longitude) * this.globeRadius;
